31c5093b0b74de1a8c526cd6a338d09f2eba37c5
Kahu Griffin <kgr78@uclive.ac.nz>
2024-04-05 11:20:27 +1300

    writing more tests just trying to figure out how to show test coverage.

--- testing/test_configReader.py
+++ testing/test_configReader.py
@@ -5,7 +5,6 @@ def test_parse_config_file():
     config = ConfigReader()
     config.parseConfigFile('../configFile/config1.txt')
     assert config.config == {'router-id': '1', 'input-ports': '6000, 6001, 6002', 'outputs': '6003-1-2, 6004-8-7, '
-
                                                                                              '6005-5-6'}
 
 
@@ -14,7 +13,6 @@ def test_validate_config():
     config.parseConfigFile('../configFile/config1.txt')
     config.validateConfig()
     assert config.config == {'router-id': '1', 'input-ports': '6000, 6001, 6002', 'outputs': '6003-1-2, 6004-8-7, '
-                
                                                                                              '6005-5-6'}
 
 
@@ -26,3 +24,92 @@ def test_invalid_config_name():
         assert True
     else:
         assert False
+
+
+def test_invalid_input_port():
+    config = ConfigReader()
+    invalidConfigFile = {'router-id': '1', 'input-ports': '1, 6001, 6002', 'outputs': '6003-1-2, 6004-8-7, '
+                                                                                      '6005-5-6'}
+    config.config = invalidConfigFile
+    try:
+        config.validateConfig()
+    except SystemExit:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_output_port():
+    config = ConfigReader()
+    invalidConfigFile = {'router-id': '1', 'input-ports': '6000, 6001, 6002', 'outputs': '1-1-2, 6004-8-7, '
+                                                                                         '6005-5-6'}
+    config.config = invalidConfigFile
+    try:
+        config.validateConfig()
+    except SystemExit:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_param_number():
+    config = ConfigReader()
+    invalidConfigFile = {'router-id': '1', 'input-ports': '6000, 6001, 6002'}
+    config.config = invalidConfigFile
+    try:
+        config.validateConfig()
+    except SystemExit:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_router_id():
+    config = ConfigReader()
+    invalidConfigFile = {'router-id': '64001', 'input-ports': '6000, 6001, 6002', 'outputs': '6003-1-2, 6004-8-7, '
+                                                                                             '6005-5-6'}
+    config.config = invalidConfigFile
+    try:
+        config.validateConfig()
+    except SystemExit:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_output_router_id():
+    config = ConfigReader()
+    invalidConfigFile = {'router-id': '1', 'input-ports': '6000, 6001, 6002', 'outputs': '6003-1-64001, 6004-8-7, '
+                                                                                             '6005-5-6'}
+    config.config = invalidConfigFile
+    try:
+        config.validateConfig()
+    except SystemExit:
+        assert True
+    else:
+        assert False
+
+
+def test_correct_router_id():
+    config = ConfigReader()
+    config.parseConfigFile('../configFile/config1.txt')
+    config.validateConfig()
+    assert config.getRouterId() == 1
+
+
+def test_correct_input_ports():
+    config = ConfigReader()
+    config.parseConfigFile('../configFile/config1.txt')
+    config.validateConfig()
+    assert config.getInputPorts() == [6000, 6001, 6002]
+
+
+def test_correct_outputs():
+    config = ConfigReader()
+    config.parseConfigFile('../configFile/config1.txt')
+    config.validateConfig()
+    assert config.getOutputs() == [(6003, 1, 2), (6004, 8, 7), (6005, 5, 6)]
+
+
+
+
