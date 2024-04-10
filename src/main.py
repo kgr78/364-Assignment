473906c2910bbe50cbec35d89cfbe3e7711c4486
Kahu Griffin <kgr78@uclive.ac.nz>
2024-04-10 19:29:54 +1200

    This way currently works but can be improved.

--- src/main.py
+++ src/main.py
@@ -35,6 +35,8 @@ def main():
             router_id, input_ports, outputs = run_checks_and_get_values(config_file)
             router_info[router_id] = {'input_ports': input_ports, 'outputs': outputs}
             RouterInterface(input_ports)
+            rip_protocol = RIPProtocol(router_info)
+            rip_protocol.start_listening()
             print(f"Router {router_id} input ports: {input_ports} bound successfully.")
         except FileNotFoundError:
             print(f"Error: Configuration file '{config_file}' not found.")
@@ -43,10 +45,6 @@ def main():
             sys.exit(1)
         except Exception as e:
             print(f"Error processing configuration file '{config_file}': {e}")
-
-    # Start the RIPProtocol to listen for incoming data on input ports
-    rip_protocol = RIPProtocol(router_info)
-    rip_protocol.start_listening()
 
 
 if __name__ == "__main__":
